af30b8e90980b8c3c6b6c8dfe13a404deed756a8
vineetnv <vineetvadrevu@gmail.com>
2024-11-05 00:30:04 -0600

    Update and rename ConsStatMethod_Extreme to StatisticsModel_nYears.py

--- StatisticsModel_nYears.py
+++ StatisticsModel_nYears.py
@@ -1,16 +1,20 @@
 import pandas as pd
 import statistics
 import matplotlib.pyplot as pl
+
 file_path="C:/Users/Krishna Vadrevu/Desktop/Vineet_Stuff/datasets/firedatakpv/srilanka_2012-2023_peaksummer_only_daily_aggregate.xlsx"
+
 #file_path="C:/Users/Krishna Vadrevu/Desktop/Vineet_Stuff/Datasets/afghanistan_2012-2023_peaksummer_only_daily_aggregate.xlsx"
 #file_path="C:/Users/Krishna Vadrevu/Desktop/Vineet_Stuff/Datasets/New_bhutan_2012-2023_peaksummer_only_daily_aggregate (3) (1).xlsx"
+
 oversum=0
 country="Bangladesh"
 arr=[]
 arrtime=[]
 choice=0
 cstring=""
-while(1):
+
+while(1): #user input
     choice=input("Please pick an option Fire Counts(1), or FRP Mean(2)")
     choice=int(choice)
     if(choice==1):
@@ -24,7 +28,8 @@ while(1):
     else:
         print("Please enter a valid option");continue
 
-file=pd.ExcelFile(file_path)
+
+file=pd.ExcelFile(file_path) # find number of extreme events in any given year for any given country OR find the FRP Mean over the time period.
 for sheet in file.sheet_names:
     print(sheet,"\n")
     df = pd.read_excel(file, sheet_name=sheet,usecols=[choice])#2
@@ -55,6 +60,8 @@ for sheet in file.sheet_names:
 
 
 print("In total, there were",oversum,"events.")
+
+#plot the data
 dat={}
 for x in range(len(arr)):
     dat[arrtime[x]]=arr[x]
@@ -62,9 +69,9 @@ dat["Total Events"]=oversum
 print(dat)
 dfplot = pd.DataFrame(list(dat.items()), columns=['Year', cstring])
 fig, ax = pl.subplots(figsize=(1,4))
-pl.title("Afghanistan",pad=35)
+pl.title("Afghanistan",pad=35) #specifically testing Afghanistan, but can replace with var = country 
 table = ax.table(cellText=dfplot.values, colLabels=dfplot.columns, cellLoc='center', loc='center')
-dfplot=dfplot.T
+dfplot = dfplot.T
 ax.axis('off')
 table.set_fontsize(20)
 table.scale(0.75,0.75)
@@ -75,5 +82,6 @@ print(dat)
 dat=dat.T
 dat.reset_index(inplace=True)
 print("-------------------------------------------------------------------")
+#export data to external excel file
 with pd.ExcelWriter("C:/Users/Krishna Vadrevu/Desktop/Vineet_Stuff/Datasets/InWrit.xlsx") as writer:
     dat.to_excel(writer, sheet_name="Sheet1",index=False)
